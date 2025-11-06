9ca4cfd68e76c2028d754762eee7222c49b0f675
Solo-2414 <danielrodge.caindoy@gmail.com>
2025-11-06 21:15:53 +0800

    Apritado, Allyssa Grace A., Dimaculangan, Marc Denver L. - updated class room number value

--- schema.sql
+++ schema.sql
@@ -9,3 +9,4 @@ CREATE TABLE classes (
 id INT AUTO_INCREMENT PRIMARY KEY, class_name VARCHAR(50), room_no INT);
 INSERT INTO classes (id, class_name,room_no) VALUES (1, Physics Lab, 501);
 ALTER TABLE classes ADD COLUMN teacher_id INT, ADD CONSTRAINT fk_teacher_id FOREIGN KEY (teacher_id) REFERENCES teachers(id);
+UPDATE classes SET room_no = 101 WHERE id = 1;
