c023793644a5a37fcb268ee81cbd1f7fd47707a9
shibyyyy <mendozaraine0@gmail.com>
2025-11-07 16:49:00 +0800

    Hannah Callelero added a column 'age'

--- schema.sql
+++ schema.sql
@@ -7,3 +7,4 @@ grade INT
 );
 Create table students (id int auto_increment primary key, name varchar(100), grade_level varchar(50));
 Insert into students(name,grade_level) values ('Althea', '2nd Year'),('Marie', '2nd Year'),('Jacob', '1st Year');
+Alter table students add age int;
