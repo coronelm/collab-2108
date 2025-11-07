d914207ca3b404ea512280cd8531aacc163f6540
shibyyyy <mendozaraine0@gmail.com>
2025-11-07 16:57:30 +0800

    Eacee Hernandez updated Althea's age

--- schema.sql
+++ schema.sql
@@ -8,3 +8,4 @@ grade INT
 Create table students (id int auto_increment primary key, name varchar(100), grade_level varchar(50));
 Insert into students(name,grade_level) values ('Althea', '2nd Year'),('Marie', '2nd Year'),('Jacob', '1st Year');
 Alter table students add age int;
+Update students set age = 20 where name = 'Althea';
