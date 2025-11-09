8126b6e425d16a9cdf4a91dc988143534a8446b5
Godwin Nunez <nunezgodwin@gmail.com>
2025-11-09 12:25:53 +0800

    Group9-Mhikhale: Updated one user's email

--- schema.sql
+++ schema.sql
@@ -8,3 +8,4 @@ grade INT
 CREATE TABLE users (user_id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(50), role VARCHAR(20));
 INSERT INTO users (username, role) VALUES ('admin1', 'Admin'), ('student1', 'Student');
 ALTER TABLE users ADD COLUMN email VARCHAR(100);
+UPDATE users SET email='admin1@email.com' WHERE username='admin1';
