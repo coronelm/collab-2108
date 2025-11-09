36a67d8e25744ad7181c0695e16c8346e1c62c2e
Godwin Nunez <nunezgodwin@gmail.com>
2025-11-09 12:24:00 +0800

    Group9-Zairah: Inserted admin and student users

--- schema.sql
+++ schema.sql
@@ -6,3 +6,4 @@ name VARCHAR(100),
 grade INT 
 );
 CREATE TABLE users (user_id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(50), role VARCHAR(20));
+INSERT INTO users (username, role) VALUES ('admin1', 'Admin'), ('student1', 'Student');
