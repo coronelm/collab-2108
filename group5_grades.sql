b707b665c28b6a545a3ca1051ade2d9987941b20
unknown <fesaritcevastian@gmail.com>
2025-11-10 21:15:24 +0800

    Dayne: inserted sample grades.

--- group5_grades.sql
+++ group5_grades.sql
@@ -1,1 +1,1 @@
-CREATE TABLE grades ( grade_id INT AUTO_INCREMENT PRIMARY KEY, student_id INT, subject_id INT, score DECIMAL(5,2));
+INSERT INTO grades (grade_id, student_id, subject_id, score) VALUES (01,001, 0001, 88), (02, 002, 0002, 81), (03, 003, 0003, 99), (04, 004, 0004, 75), (05, 005, 0005, 93);
