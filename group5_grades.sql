c48663a97bf09e6e617680e7691a645c0db0f636
unknown <fesaritcevastian@gmail.com>
2025-11-10 21:24:41 +0800

    John Dave: inserted sample grades.

--- group5_grades.sql
+++ group5_grades.sql
@@ -1,1 +1,1 @@
-INSERT INTO grades (grade_id, student_id, subject_id, score) VALUES (01,001, 0001, 88), (02, 002, 0002, 81), (03, 003, 0003, 99), (04, 004, 0004, 75), (05, 005, 0005, 93);
+ALTER TABLE grades ADD COLUMN remarks VARCHAR(50);
