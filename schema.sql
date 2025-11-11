c5b2a0b1d2a9a56e183e0914a1e0915bc6b68a3c
Cy <clydemagnaye2@gmail.com>
2025-11-11 17:39:59 +0800

    Gian added department column

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE TABLE teachers (id INT, name VARCHAR(100), subject VARCHAR(100));
 insert into teachers (id, name, subject) VALUES (1, 'Gian', 'Intro'), (2, 'Irvin', 'ArtApp'), (3, 'Tirao', 'Fili');
+ALTER TABLE teachers ADD COLUMN email VARCHAR(100);
