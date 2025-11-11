87583d97e5e260a6de78c86cfa7921f94eff302c
Cy <clydemagnaye2@gmail.com>
2025-11-11 17:30:39 +0800

    Marlorenz inserted 3 teachers

--- schema.sql
+++ schema.sql
@@ -1,1 +1,2 @@
 CREATE TABLE teachers (id INT, name VARCHAR(100), subject VARCHAR(100));
+insert into teachers (id, name, subject) VALUES (1, 'Gian', 'Intro'), (2, 'Irvin', 'ArtApp'), (3, 'Tirao', 'Fili');
