74eb3198bb53371f05c23c315c6b777e1c673216
Cy <clydemagnaye2@gmail.com>
2025-11-11 17:47:46 +0800

    Simon updated one teacher's department.

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE TABLE teachers (id INT, name VARCHAR(100), subject VARCHAR(100));
 insert into teachers (id, name, subject) VALUES (1, 'Gian', 'Intro'), (2, 'Irvin', 'ArtApp'), (3, 'Tirao', 'Fili');
 ALTER TABLE teachers ADD COLUMN email VARCHAR(100);
+UPDATE teachers SET department='Science' WHERE id=1;
