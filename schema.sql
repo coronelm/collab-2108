927a320c51d5d03fb11d058cafff2258ce7db6b6
Jol Cyndrelle Q. Bajenting <24-36499@g.batstate-u.edu.ph>
2025-11-11 22:40:40 +0800

    Reyes, Jean Kyzell P. updated CICS department

--- schema.sql
+++ schema.sql
@@ -3,3 +3,4 @@ CREATE TABLE department (department_id INT AUTO INCREMENT PRIMARY KEY, departmen
 INSERT INTO department (department_id, department_name)VALUES (01,'CICS'),(02,'CABE'),(03,'CET');
 INSERT INTO department (department_id, department_name)VALUES (01,'CICS'),(02,'CABE'),(03,'CET');
 ALTER TABLE department ADD COLUMN head_teacher VARCHAR(50);
+UPDATE Department SET head_teacher = 'Liza Bernardo' WHERE department_name = 'CICS';
