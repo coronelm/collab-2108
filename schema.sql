08d1b4d6979a4a68478219ac49b25743ee1866c5
Jol Cyndrelle Q. Bajenting <24-36499@g.batstate-u.edu.ph>
2025-11-11 22:25:18 +0800

    Namuco, Leslie Jane V. added head_teacher column

--- schema.sql
+++ schema.sql
@@ -2,3 +2,4 @@ CREATE TABLE subject (subject_id INT AUTO INCREMENT PRIMARY KEY, subject_name VA
 CREATE TABLE department (department_id INT AUTO INCREMENT PRIMARY KEY, department_name VARCHAR(50);
 INSERT INTO department (department_id, department_name)VALUES (01,'CICS'),(02,'CABE'),(03,'CET');
 INSERT INTO department (department_id, department_name)VALUES (01,'CICS'),(02,'CABE'),(03,'CET');
+ALTER TABLE department ADD COLUMN head_teacher VARCHAR(50);
