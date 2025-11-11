d8108afe52ae940df7618a0aea4aea0dc72e6542
Jol Cyndrelle Q. Bajenting <24-36499@g.batstate-u.edu.ph>
2025-11-11 21:56:20 +0800

    Bajenting, Jol Cyndrelle Q. created department table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,2 @@
 CREATE TABLE subject (subject_id INT AUTO INCREMENT PRIMARY KEY, subject_name VARCHAR(50);
+CREATE TABLE department (department_id INT AUTO INCREMENT PRIMARY KEY, department_name VARCHAR(50);
