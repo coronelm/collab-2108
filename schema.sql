f4b5935f178bba3f316bc3d84d2eb7d268378fc6
jibi79 <johnbrielhernandez@gmail.com>
2025-11-12 22:40:51 +0800

    Glend Dimaano added school_year column

--- schema.sql
+++ schema.sql
@@ -9,3 +9,4 @@ Create enrollments table (enrollment_id INT, student_id INT, class_id INT);
 Insert into enrollments (enrollment_id, student_id, class_id) values (001, 123, 08);
 Alter Table enrollments add column school_year VARCHAR(50);
 Update enrollments SET school_year = '2024-2025' WHERE enrollment_id = '001';
+Alter Table enrollments add column school_year VARCHAR(50);
