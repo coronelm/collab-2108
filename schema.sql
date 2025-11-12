d2ee5ce17735d0ce66b7367aa1dd34497d5886e0
flowzies <tristan.78.tm@gmail.com>
2025-11-12 19:50:58 +0800

    PAUL TENORIO ADDED THE STUDENT RECORD EXAMPLES

--- schema.sql
+++ schema.sql
@@ -4,3 +4,7 @@ CREATE TABLE attendance (
   date DATE NOT NULL,
   status VARCHAR(20)
 );
+INSERT INTO attendance (attendance_id, student_id, date, status) VALUES
+(001, 1, '2025-11-12', 'Present'),
+(102, 2, '2025-11-12', 'Absent'),
+(002, 3, '2025-11-12', 'Present')
